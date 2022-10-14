6a9d87dd2d5b8572729d2176650f8e48cbf1a26a
unknown <juslepi@gmail.com>
2022-10-14 17:05:00 +0300

    Login UI

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import LoginModal from "./components/LoginModal";
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(true)
+
   return (
     <div className="App">
       <Header />
+      {loggedIn ? <LoginModal /> : null}
     </div>
   );
 }
---
+++ front/src/components/LoginModal.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
+
+const LoginModal = () => {
+  const [formDetails, setformDetails] = useState({
+    email: "",
+    password: "",
+  });
+
+  const submit = () => {
+    throw new Error("Not implemented")
+  }
+
+  return (
+    <Modal show={true}>
+      <Modal.Header closeButton>
+        <Modal.Title>Log In</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Email"
+              value={formDetails.email}
+              onChange={(e) =>
+                setformDetails({ ...formDetails, email: e.target.value })
+              }
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="password"
+              value={formDetails.password}
+              onChange={(e) =>
+                setformDetails({ ...formDetails, password: e.target.value })
+              }
+            ></Form.Control>
+          </Form.Group>
+          <Button variant="dark" type="submit" onClick={submit}>
+            Submit
+          </Button>
+        </Form>
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default LoginModal;
