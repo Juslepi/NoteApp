9c0c587bf762e69683e99403ed62dc2813e4c68c
unknown <juslepi@gmail.com>
2022-10-16 02:54:07 +0300

    init schema

---
+++ back/schemas/note.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const noteSchema = new Schema({
+    name: String,
+    content: String,
+    date: Date,
+});
+
+const noteModel = mongoose.model("Note", noteSchema);
+
+module.exports = noteModel;
