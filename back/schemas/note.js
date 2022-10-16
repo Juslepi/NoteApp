a3ffdd51c5b7aa7f56e9d082d6478f0bc86c1788
unknown <juslepi@gmail.com>
2022-10-16 22:10:12 +0300

    content set required

--- back/schemas/note.js
+++ back/schemas/note.js
@@ -1,13 +1,13 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const noteSchema = new Schema({
     name: String,
-    content: String,
+    content: { type: String, required: true },
     date: String,
 });
 
 const noteModel = mongoose.model("Note", noteSchema);
 
-module.exports = noteModel
+module.exports = noteModel;
