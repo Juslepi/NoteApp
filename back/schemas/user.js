466ec3ac9c16d871e90b44ab262da9d34d8bddc4
unknown <juslepi@gmail.com>
2022-10-17 04:20:59 +0300

    remove password req

--- back/schemas/user.js
+++ back/schemas/user.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
     username: { type: String, required: true },
-    password: { type: String, required: true },
+    password: String
 });
 
 const userModel = mongoose.model("User", userSchema);
