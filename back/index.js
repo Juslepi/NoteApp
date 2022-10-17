a9434b1b122fa0612803836bde9ea3b6ca984461
unknown <juslepi@gmail.com>
2022-10-17 18:25:16 +0300

    auth middleware

--- back/index.js
+++ back/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { authMiddleWare } = require("./middleware/authorization");
 require("dotenv").config();
 const { noteRouter } = require("./routes/notes");
 const { userRouter } = require("./routes/user");
@@ -7,6 +8,8 @@ const app = express();
 app.use(express.json());
 app.use("/note", noteRouter)
 app.use("/user", userRouter)
+
+app.use("/secret", authMiddleWare)
 
 const port = process.env.PORT;
 app.listen(port, () => console.log("Listening to ", port));
--- back/middleware/authorization.js
+++ back/middleware/authorization.js
@@ -1,1 +1,25 @@
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
+const router = require("express").Router();
+require("dotenv").config();
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const verifyToken = async (req, res, next) => {
+    const token = req.headers.authorization.split(" ")[1];
+
+    if (token === null || token === undefined) {
+        res.status(403).send({ msg: "No token found" });
+    }
+
+    try {
+        const tokenVerified = await jwt.verify(token, JWT_SECRET);
+        res.locals.username = tokenVerified.username;
+    } catch (e) {
+        res.send({ msg: "Invalid Token" });
+        return;
+    }
+    
+    next();
+};
+
+module.exports = { authMiddleWare: router };
