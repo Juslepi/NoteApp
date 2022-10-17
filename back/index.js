41dd918cb65e8dff053183051d0da6550f8e25af
unknown <juslepi@gmail.com>
2022-10-17 18:26:10 +0300

    remove tests

--- back/index.js
+++ back/index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { authMiddleWare } = require("./middleware/authorization");
 require("dotenv").config();
 const { noteRouter } = require("./routes/notes");
 const { userRouter } = require("./routes/user");
@@ -8,8 +7,6 @@ const app = express();
 app.use(express.json());
 app.use("/note", noteRouter)
 app.use("/user", userRouter)
-
-app.use("/secret", authMiddleWare)
 
 const port = process.env.PORT;
 app.listen(port, () => console.log("Listening to ", port));
