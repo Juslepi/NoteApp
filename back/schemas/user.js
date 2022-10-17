43b7feb77e650a71c57d5438c7f56839d5d377e4
unknown <juslepi@gmail.com>
2022-10-17 03:58:07 +0300

    fix required fields

--- back/schemas/user.js
+++ back/schemas/user.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: { type: String, required },
-    password: { type: String, required },
+    username: { type: String, required: true },
+    password: { type: String, required: true },
 });
 
 const userModel = mongoose.model("User", userSchema);
