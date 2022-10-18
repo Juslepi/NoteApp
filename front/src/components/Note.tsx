8496655c3b531e41f422d487608790fee7b1dc00
unknown <juslepi@gmail.com>
2022-10-18 11:10:43 +0300

    note height

--- front/src/components/Note.tsx
+++ front/src/components/Note.tsx
@@ -2,26 +2,32 @@ import { FC } from "react";
 import Container from "react-bootstrap/Container";
 
 export interface INote {
-  name: string;
-  content: string;
-  _id: string;
+    name: string;
+    content: string;
+    _id: string;
 }
 
 export type NoteProps = {
-  note: INote;
-  onClickNote: (e: any, note: INote) => void;
+    note: INote;
+    onClickNote: (e: any, note: INote) => void;
 };
 
 const Note: FC<NoteProps> = ({ note, onClickNote }) => {
-  return (
-    <Container
-      className="bg-warning p-3 m-3 w-25 shadow rounded "
-      onClick={(e) => onClickNote(e, note)}
-    >
-      <h6>{note.name}</h6>
-      <p className="">{note.content}</p>
-    </Container>
-  );
+    return (
+        <Container
+            className="bg-warning p-3 m-3 w-25 shadow rounded "
+            onClick={(e) => onClickNote(e, note)}
+            style={{
+                maxHeight: "270px",
+                overflow:"hidden",
+            }}
+        >
+            <h6>{note.name}</h6>
+            <Container className="p-1">
+                <p>{note.content}</p>
+            </Container>
+        </Container>
+    );
 };
 
 export default Note;
